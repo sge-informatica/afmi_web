f954a6710a85811f8ae765d627db0ba5d32ae9f8
leolivm <59733804+leolivm@users.noreply.github.com>
2020-02-12 16:40:28 -0300

    recover-password-fix

--- src/services/api.js
+++ src/services/api.js
@@ -6,7 +6,7 @@ const api = axios.create({
 
 export const redirect_url = () =>
   process.env.NODE_ENV === "development"
-    ? "http://localhost:3000/recover-password"
+    ? "https://afmi-convenios.netlify.com/recover-password"
     : "https://api.sgeinformatica.com.br/afmi/passwords";
 
 export default api;
